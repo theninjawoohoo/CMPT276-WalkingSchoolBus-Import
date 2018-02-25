752b063465143228279d4d509382123fbac1fa86
Andy W <wuandyw@sfu.ca>
2018-02-24 18:12:11 -0800

    Initialized Project.

---
+++ app/src/main/java/groupdenim/cmpt276/awalkingschoolbus/MainActivity.java
@@ -0,0 +1,13 @@
+package groupdenim.cmpt276.awalkingschoolbus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
