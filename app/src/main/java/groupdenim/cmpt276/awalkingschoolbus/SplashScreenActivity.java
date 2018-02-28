f6b850aacef0757ff3a9cde3e554493bf23e8a88
fchandel <fchandel@sfu.ca>
2018-02-27 22:57:03 -0800

    Adds Login Acitivty template. As Well As Splash Screen Animation.
    REQUIRES CHANGE TO MANIFEST: To set Splash Screen As Launcher Activity.

---
+++ app/src/main/java/groupdenim/cmpt276/awalkingschoolbus/SplashScreenActivity.java
@@ -0,0 +1,49 @@
+package groupdenim.cmpt276.awalkingschoolbus;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        //displayAnimation();
+        setAnimationListener();
+
+    }
+
+    private void setAnimationListener() {
+        ImageView logo = findViewById(R.id.logo);
+        Animation fade = AnimationUtils.loadAnimation(this,R.anim.fade);
+        logo.startAnimation(fade);
+        fade.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    private void displayAnimation() {
+        ImageView logo = findViewById(R.id.logo);
+        Animation fade = AnimationUtils.loadAnimation(this,R.anim.fade);
+        logo.startAnimation(fade);
+    }
+}
