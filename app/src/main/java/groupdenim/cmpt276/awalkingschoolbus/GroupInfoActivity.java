7fed35d4dc7b3eb00c9b9f040497b7e3717aa182
ajoziak <ajoziak@sfu.ca>
2018-03-02 21:36:59 -0800

    Created GroupInfoActivity for displaying information about a specific group.

---
+++ app/src/main/java/groupdenim/cmpt276/awalkingschoolbus/GroupInfoActivity.java
@@ -0,0 +1,44 @@
+package groupdenim.cmpt276.awalkingschoolbus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class GroupInfoActivity extends AppCompatActivity {
+    private String tempName = "GroupName";
+    private String[] tempMembers = {"Name1", "Name2", "Name3", "Name4", "Name5", "Name6",
+            "Name7", "Name8", "Name9", "Name10", "Name11", "Name12", "Name12", "Name14", "Name15"};
+    private String tempDestination = "Destination School";
+    private String tempMeeting = "Meeting Street";
+    private final float TEXT_SIZE = 24;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_info);
+        populateFields(tempName, R.id.hlinear_layout_group_name);
+        populateFields(tempDestination, R.id.hlinear_layout_group_destination);
+        populateFields(tempMeeting, R.id.hlinear_layout_group_meeting);
+        populateList();
+    }
+
+    private void populateFields(String name, int resId) {
+        LinearLayout layout = findViewById(resId);
+        TextView text = new TextView(this);
+        text.setText(name);
+        text.setTextSize(TEXT_SIZE);
+        text.setGravity(Gravity.LEFT);
+        layout.addView(text);
+    }
+
+    private void populateList(){
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                R.layout.group_member, tempMembers);
+        ListView list = findViewById(R.id.list_members);
+        list.setAdapter(adapter);
+    }
+}
