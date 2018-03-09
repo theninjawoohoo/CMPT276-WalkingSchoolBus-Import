d2432b8f1ba5aa8c4424f29c7be7fe96558f70dc
ajoziak <ajoziak@sfu.ca>
2018-03-08 20:17:58 -0800

    Coordinate class.

---
+++ app/src/main/java/groupdenim/cmpt276/awalkingschoolbus/Coordinate.java
@@ -0,0 +1,27 @@
+package groupdenim.cmpt276.awalkingschoolbus;
+
+public class Coordinate {
+    private int X;
+    private int Y;
+
+    public Coordinate(int X, int Y) {
+        this.X = X;
+        this.Y = Y;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public void setX(int X) {
+        this.X = X;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public void setY(int Y) {
+        this.Y = Y;
+    }
+}
