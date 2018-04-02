dd76d24941465e94829be8d55791ae0ec62ddd64
niragmehta <nsmehta@sfu.ca>
2018-04-02 14:01:39 -0700

    created the gamification and quiz class

--- app/src/main/java/groupdenim/cmpt276/awalkingschoolbus/userModel/Gamification.java
+++ app/src/main/java/groupdenim/cmpt276/awalkingschoolbus/userModel/Gamification.java
@@ -1,10 +1,103 @@
 package groupdenim.cmpt276.awalkingschoolbus.userModel;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by niragmehta on 2018-04-02.
  */
 
 public class Gamification {
 
+    private int quizID;
+    private int imageID;
+    private int titleID;
+    private int points;
+    List<Integer> quizzesSolved=new ArrayList<>();
+    boolean quizAttempted=false;
+    Date date;
 
+    //singleton object
+    private static Gamification instance;
+
+    public static Gamification getInstance()
+    {
+        if(instance==null)
+            instance=new Gamification();
+        return instance;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Gamification{" +
+                "quizID=" + quizID +
+                ", imageID=" + imageID +
+                ", titleID=" + titleID +
+                ", points=" + points +
+                ", quizzesSolved=" + quizzesSolved.toString() +
+                ", quizAttempted=" + quizAttempted +
+                ", date=" + date +
+                '}';
+    }
+
+
+
+    //getters and setters
+    public int getQuizID() {
+        return quizID;
+    }
+
+    public void setQuizID(int quizID) {
+        this.quizID = quizID;
+    }
+
+    public int getImageID() {
+        return imageID;
+    }
+
+    public void setImageID(int imageID) {
+        this.imageID = imageID;
+    }
+
+    public int getTitleID() {
+        return titleID;
+    }
+
+    public void setTitleID(int titleID) {
+        this.titleID = titleID;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public List<Integer> getQuizzesSolved() {
+        return quizzesSolved;
+    }
+
+    public void setQuizzesSolved(List<Integer> quizzesSolved) {
+        this.quizzesSolved = quizzesSolved;
+    }
+
+    public boolean isQuizAttempted() {
+        return quizAttempted;
+    }
+
+    public void setQuizAttempted(boolean quizAttempted) {
+        this.quizAttempted = quizAttempted;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
---
+++ app/src/main/java/groupdenim/cmpt276/awalkingschoolbus/userModel/Quiz.java
@@ -0,0 +1,94 @@
+package groupdenim.cmpt276.awalkingschoolbus.userModel;
+
+/**
+ * Created by niragmehta on 2018-04-02.
+ */
+
+public class Quiz {
+
+
+
+    public static final String[] questions={"What is the world's largest desert?",
+            "Where is Mount Everest located?",
+            "Where is the Eiffel tower located?",
+            "Which sport is Mike Tyson known for",
+            "What device is used to measure altitude",  //5
+            "Deficiency of iron leads to?",
+            "Who is the author of the Harry Potter series",//7
+            "The flag of which country has the star of david?",
+            "Which city is the capital of Canada?", //9
+            "Which of the following is not a gas?",
+            "Which of the following countries are landlocked?",
+            "Which sport is Lionel Messi known for?",   //12
+            "Where is the machu pichu located",
+            "Who is the author of 'The Lord of the Rings' ",//14
+            "Which monarch did Henry VIII belong to?"};     //15
+
+    public final static String[] optionA={"Gobi Desert",
+    "China",
+    "Germany",
+    "Karate",
+    "barometer",
+    "anaemia",  //6
+    "JK Rowling",
+    "Lebanon",
+    "Toronto",
+    "Hydrogen",
+    "Switzerland",
+    "Soccer",
+    "Brazil",
+    "George RR Martin",
+    "Medici"};
+
+    public final static String[] optionB={"Sahara Desert",
+    "India",
+    "Netherlands",
+    "Jiu Jitsu",
+    "speedometer",
+    "scurvy",
+    "George RR Martin",
+    "United States",
+    "Vancouver",
+    "Mercury",
+    "Mali",
+    "Rugby",
+    "Argentina",
+    "Sidney Sheldon",
+    "Tudor"};
+
+    public final static String[] optionC={"Atacama Desert",
+    "Nepal",
+    "Italy",
+    "boxing",
+    "altimeter",
+    "hepatitis",
+    "christopher paolini",
+    "Iran",
+    "Ottawa",
+    "Argon",
+    "Nigeria",
+    "Baseball",
+    "Venezuela",
+    "JRR Tolkien",
+    "Borgias"};     //15
+
+    public final static String[] optionD={"Kalahari Desert",
+    "Bhutan",
+    "France",
+    "wrestling",
+    "thermometer",
+    "pneumonia",
+    "Stephen King",
+    "Israel",
+    "Montreal",
+    "Neon",
+    "Bangladesh",
+    "Basketball",
+    "Peru",
+    "Neil Gaiman",
+    "House of Lancaster"};
+                                               //6 7 8 9 10 11 12 13  14 15
+    public static final int[] answers={2,3,4,3,3,1,1,4,3,2,  2, 1, 4, 3,  3};
+
+
+}
